f0325455e2c89f96bb54c27e6d46bf2e45d2dc05
Atul Falle <atulfalle007@gmail.com>
2026-08-15 22:50:38 +0530

    theme selector implementation

--- src/app/docs/layout/doc-layout.ts
+++ src/app/docs/layout/doc-layout.ts
@@ -1,14 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { DOC_NAV } from '../nav';
+import { DocsThemeService } from '../theme-generator/docs-theme.service';
+import { ThemePanel } from '../theme-generator/theme-panel';
 
 @Component({
   selector: 'app-doc-layout',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, ThemePanel],
   templateUrl: './doc-layout.html',
   styleUrl: './doc-layout.scss',
 })
 export class DocLayout {
   protected readonly nav = DOC_NAV;
+  protected readonly docsTheme = inject(DocsThemeService);
+  protected readonly themeOpen = signal(false);
 }
